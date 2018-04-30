5ed8232a99fb8adf20df4541fceb0c0fcdf5afb8
Rade Krstanovic <radekrstanovic@gmail.com>
2018-04-30 10:34:25 +0200

    Add UserRepository

---
+++ src/main/java/com/series/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.series.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.series.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
