7e5ae5fbad9b4521c23605783e9ad8fdeaff43f7
Rade Krstanovic <radekrstanovic@gmail.com>
2018-04-30 16:29:06 +0200

    Add exception handling

---
+++ src/main/java/com/series/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.series.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.series.exception.ResourceNotFoundException;
+import com.series.model.User;
+import com.series.repository.UserRepository;
+
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping("/users")
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+	
+	@PostMapping("/users")
+	public User createUser(@Valid @RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	@GetMapping("/users/{id}")
+	public User getUserById(@PathVariable(value = "id") Long userId) {
+		return userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+	}
+	
+	@PutMapping("/users/{id}")
+	public User updateUser(@PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetails) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+		user.setUsername(userDetails.getEmail());
+		user.setPassword(userDetails.getPassword());
+		user.setEmail(userDetails.getEmail());
+		
+		User updatedUser = userRepository.save(user);
+		return updatedUser;
+	}
+}
