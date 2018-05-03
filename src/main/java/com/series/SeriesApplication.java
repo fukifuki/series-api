b1939f2ea88dc1f740a08c5f4042136ec02f8303
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-03 10:40:05 +0200

    Enable JPA auditing

--- src/main/java/com/series/SeriesApplication.java
+++ src/main/java/com/series/SeriesApplication.java
@@ -2,8 +2,10 @@ package com.series;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SeriesApplication {
 
 	public static void main(String[] args) {
