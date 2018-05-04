059e4af409279c2391cb4103f0a19f7ef0d7fa1f
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-04 12:22:51 +0200

    Fix typos

--- src/main/java/com/series/model/User.java
+++ src/main/java/com/series/model/User.java
@@ -21,7 +21,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name = "series")
+@Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable {
 	
