e2cf4bfc40028e198f69faf5e2d5222aba1e473d
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-08 10:25:47 +0200

    Add basic security configuration

--- src/main/java/com/series/WebSecurityConfig.java
+++ src/main/java/com/series/WebSecurityConfig.java
@@ -1,26 +1,39 @@
 package com.series;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.series.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService);
+	}
+	
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+//		ignore assets here...
+	}
+	
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/users/**").permitAll()
-				.anyRequest().authenticated()
+				.anyRequest().hasRole("USER")
 				.and()
 			.formLogin()
 				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
 				.permitAll();
 	}
 	
