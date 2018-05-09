5055e1458b0a6d47afaf9ea418513fef906d413a
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-09 13:54:49 +0200

    Add matchers for authorizing requests

--- src/main/java/com/series/WebSecurityConfig.java
+++ src/main/java/com/series/WebSecurityConfig.java
@@ -2,11 +2,15 @@ package com.series;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.series.service.UserService;
 
@@ -19,7 +23,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userService);
+		auth.userDetailsService(userService)
+		.passwordEncoder(new PasswordEncoder() {
+			@Override
+			public String encode(CharSequence charSequence) {
+				return charSequence.toString();
+			}
+			
+			public boolean matches(CharSequence charSequence, String s) {
+				return true;
+			}
+		});
 	}
 	
 	@Override
@@ -27,14 +41,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //		ignore assets here...
 	}
 	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest().hasRole("USER")
+				.antMatchers("/", "/signup").permitAll()
+				.antMatchers(HttpMethod.GET, "/series").permitAll()
+				.antMatchers(HttpMethod.GET, "/series/{^[\\d]$}").permitAll()
+				.antMatchers("/series/**").hasAuthority("ADMIN")
+				.anyRequest().authenticated()
 				.and()
 			.formLogin()
 				.loginPage("/login")
 				.permitAll();
+//				.successHandler(loginSuccessHandler())
+//				.failureHandler(loginFailureHandler());
 	}
+
+//	public AuthenticationSuccessHandler loginSuccessHandler() {
+//		return (request, response, authentication) -> response.sendRedirect("/");
+//	}
+	
+//	public AuthenticationFailureHandler loginFailureHandler() {
+//		return (request, response, authentication) -> {
+//			response.sendRedirect("/login");
+//		};
+//	}
 	
 }
