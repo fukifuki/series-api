aa908bf634af0c6516b7c6c64f09d347d35f3eab
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-14 13:59:20 +0200

    Add many-to-many associations with User entity (with getters and setters for lists of users who watched/who will watch a series)

--- src/main/java/com/series/model/Series.java
+++ src/main/java/com/series/model/Series.java
@@ -1,10 +1,17 @@
 package com.series.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -25,8 +32,18 @@ public class Series {
 	@NotBlank
 	private String description;
 
-	
-	
+	@ManyToMany
+	@JoinTable(name = "series_to_watch", 
+			   joinColumns = @JoinColumn(name = "series_id"),
+			   inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private Set<User> usersWhoWillWatchSeries = new HashSet<User>();
+
+	@ManyToMany
+	@JoinTable(name = "watched_series", 
+			   joinColumns = @JoinColumn(name = "series_id"),
+			   inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private Set<User> usersWhoWatchedSeries = new HashSet<User>();
+
 	public Long getId() {
 		return id;
 	}
@@ -50,5 +67,20 @@ public class Series {
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
+	public Set<User> getUsersWhoWillWatchSeries() {
+		return usersWhoWillWatchSeries;
+	}
 
+	public void setUsersWhoWillWatchSeries(Set<User> usersWhoWillWatchSeries) {
+		this.usersWhoWillWatchSeries = usersWhoWillWatchSeries;
+	}
+
+	public Set<User> getUsersWhoWatchedSeries() {
+		return usersWhoWatchedSeries;
+	}
+
+	public void setUsersWhoWatchedSeries(Set<User> usersWhoWatchedSeries) {
+		this.usersWhoWatchedSeries = usersWhoWatchedSeries;
+	}
 }
