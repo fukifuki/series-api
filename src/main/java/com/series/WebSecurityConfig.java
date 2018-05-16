e83be9ec30de567544790135a67ba9a01ade0588
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-16 16:14:15 +0200

    Add BCrypt encoder

--- src/main/java/com/series/WebSecurityConfig.java
+++ src/main/java/com/series/WebSecurityConfig.java
@@ -1,16 +1,21 @@
 package com.series;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.query.spi.EvaluationContextExtension;
+import org.springframework.data.repository.query.spi.EvaluationContextExtensionSupport;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.series.service.UserService;
 
@@ -21,19 +26,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserService userService;
 	
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userService)
-		.passwordEncoder(new PasswordEncoder() {
-			@Override
-			public String encode(CharSequence charSequence) {
-				return charSequence.toString();
-			}
-			
-			public boolean matches(CharSequence charSequence, String s) {
-				return true;
-			}
-		});
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
 	}
 	
 	@Override
@@ -44,18 +43,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable()
 			.authorizeRequests()
 				.antMatchers("/", "/signup").permitAll()
+				.antMatchers("/hello").authenticated()
+				.antMatchers(HttpMethod.POST, "/users").permitAll()
 				.antMatchers(HttpMethod.GET, "/series").permitAll()
-				.antMatchers(HttpMethod.GET, "/series/{^[\\d]$}").permitAll()
-				.antMatchers("/series/**").hasAuthority("ADMIN")
-				.anyRequest().authenticated()
+				.antMatchers(HttpMethod.GET, "/series/{^[\\d]$}").authenticated()
+//				.anyRequest().authenticated()
 				.and()
 			.formLogin()
 				.loginPage("/login")
 				.permitAll();
 //				.successHandler(loginSuccessHandler())
-//				.failureHandler(loginFailureHandler());
+//				.failureHandler(loginFailureHandler())
 	}
 
 //	public AuthenticationSuccessHandler loginSuccessHandler() {
@@ -67,5 +68,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //			response.sendRedirect("/login");
 //		};
 //	}
+	
+	@Bean
+	public EvaluationContextExtension securityExtension() {
+		return new EvaluationContextExtensionSupport() {
+			
+			@Override
+			public String getExtensionId() {
+				return "security";
+			}
+			
+			@Override
+			public Object getRootObject() {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				return new SecurityExpressionRoot(authentication) {};
+			}
+		};
+	}
 	
 }
