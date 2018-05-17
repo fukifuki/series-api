ad35fcf4f0fde9116e023f8e12cdc5206974e60d
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-17 11:36:31 +0200

    Create embeddable UserSeriesId class to represent composite key column in series_ratings table

---
+++ src/main/java/com/series/model/UserSeriesId.java
@@ -0,0 +1,32 @@
+package com.series.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+@Embeddable
+public class UserSeriesId implements Serializable {
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	private User user;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Series series;
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Series getSeries() {
+		return series;
+	}
+	public void setSeries(Series series) {
+		this.series = series;
+	}
+	
+}
