061b409e984d91298e01eda6fa208abbc7202b39
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-23 17:43:26 +0200

    Move converting between SeriesRating and SeriesRatingDto into separate convertor class

--- src/main/java/com/series/service/SeriesRatingService.java
+++ src/main/java/com/series/service/SeriesRatingService.java
@@ -1,10 +1,17 @@
 package com.series.service;
 
+import java.util.Set;
+
 import com.series.dto.SeriesRatingDto;
 import com.series.model.Series;
 import com.series.model.SeriesRating;
 import com.series.model.User;
 
 public interface SeriesRatingService {
+	
+//	Should this method expect Series or seriesId argument
+//	Actually, what I am not sure about is if I should user SeriesService even inside SeriesRatingController
+	Set<SeriesRatingDto> getSeriesRatings(Series series);
+	
 	SeriesRating rateSeries(User user, Series series, SeriesRatingDto seriesRatingDto);
 }
--- src/main/java/com/series/service/SeriesRatingServiceImpl.java
+++ src/main/java/com/series/service/SeriesRatingServiceImpl.java
@@ -1,29 +1,45 @@
 package com.series.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.series.converter.SeriesRatingConverter;
 import com.series.dto.SeriesRatingDto;
 import com.series.model.Series;
 import com.series.model.SeriesRating;
 import com.series.model.User;
 import com.series.model.UserSeriesId;
 import com.series.repository.SeriesRatingRepository;
+import com.series.repository.SeriesRepository;
 
 @Service
 public class SeriesRatingServiceImpl implements SeriesRatingService {
 
 	@Autowired
+	SeriesRatingConverter seriesRatingConverter;
+	
+	@Autowired
 	SeriesRatingRepository seriesRatingRepository;
 	
+	@Autowired
+	SeriesRepository seriesRepository;
+	
+	@Override
+	public Set<SeriesRatingDto> getSeriesRatings(Series series) {
+		
+		Set<SeriesRating> seriesRatings = series.getSeriesRatings();
+		
+		return seriesRatingConverter.createFromEntities(seriesRatings);
+	}
+	
+//	Change arguments order here!
 	@Override
 	public SeriesRating rateSeries(User user, Series series, SeriesRatingDto seriesRatingDto) {
-		SeriesRating seriesRating = new SeriesRating();
-		UserSeriesId primaryKey = new UserSeriesId();
-		primaryKey.setUser(user);
-		primaryKey.setSeries(series);
-		seriesRating.setPrimaryKey(primaryKey);
-		seriesRating.setRating(seriesRatingDto.getRating());
+		
+		SeriesRating seriesRating = seriesRatingConverter.createFromDto(seriesRatingDto, user, series);
 		
 		return seriesRatingRepository.save(seriesRating);
 	}
