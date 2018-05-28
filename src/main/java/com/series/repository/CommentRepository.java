66b6fbfb8803515469741f8ef1943877a682b1fc
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-28 13:15:13 +0200

    Remove non used imports

--- src/main/java/com/series/repository/CommentRepository.java
+++ src/main/java/com/series/repository/CommentRepository.java
@@ -1,5 +1,11 @@
 package com.series.repository;
 
-public interface CommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.series.model.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long>{
+	
 }
