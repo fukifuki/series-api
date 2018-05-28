535c434366350fe1fd0522eafab209b03862abbb
Rade Krstanovic <radekrstanovic@gmail.com>
2018-05-28 10:47:14 +0200

    Add case ignoring name finding method

--- src/main/java/com/series/repository/GenreRepository.java
+++ src/main/java/com/series/repository/GenreRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.series.model.Genre;
 
 public interface GenreRepository extends JpaRepository<Genre, Long>{
+
+//	I'll probably use only case ignoring method below...
+//	Genre findOneByName(String name);
 	
-	Genre findOneByName(String name);
-	
+	Genre findOneByNameIgnoreCase(String name);
 }
