48d8799372364347a98bd8e3466390bae9f9f02d
Rade Krstanovic <radekrstanovic@gmail.com>
2018-07-04 11:29:03 +0200

    Change WatchListsController methods so as to retrieve user by its id instead of getting it from principal

--- src/main/java/com/series/controller/WatchListsController.java
+++ src/main/java/com/series/controller/WatchListsController.java
@@ -1,6 +1,5 @@
 package com.series.controller;
 
-import java.security.Principal;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,67 +9,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.series.dto.UserDto;
 import com.series.model.Series;
-import com.series.model.User;
-import com.series.repository.UserRepository;
-import com.series.service.SeriesService;
-import com.series.service.UserService;
 import com.series.service.WatchlistsService;
 
 @RestController
-@RequestMapping
+@RequestMapping("/users/{userId}")
 public class WatchListsController {
-			
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private SeriesService seriesService;
 	
 	@Autowired
 	private WatchlistsService watchlistsService;	
-		
-//	Is there a way get principal object to use it here without having to inject it in every method?
-//	@PathVariable(value = "username") String username
+	
+	
+//  TODO Return dtos instead of enities
+ 	
+	
 	@GetMapping("/watchlist")
-	public Set<Series> getWatchListForUsername(Principal principal) {
-		
-//		Tried to get the user this way, by calling service method that calls getName() on principal object, but got NullPointer exception
-//		User user = userService.getLoggedInUser();
-		
-		UserDto user = userService.findByUsername(principal.getName()); 
-		Long userId = user.getId();
+	public Set<Series> getWatchlistForUserId(@PathVariable("userId") Long userId) {
 		
 		return watchlistsService.getWatchList(userId);
 	}
 	
-//	return value type?
 	@PostMapping("/watchlist/{seriesId}")
-	public Series addSeriesToWatchList(@PathVariable("seriesId") Long seriesId, Principal principal) {
-
-//		TODO This should be done before any method call in this controller 
-		UserDto user = userService.findByUsername(principal.getName());
-		Long userId = user.getId();
+	public Series addSeriesToWatchList(@PathVariable("userId") Long userId, @PathVariable("seriesId") Long seriesId) {
 		
 		return watchlistsService.saveToWatchlist(userId, seriesId);
 	}
 	
-	@GetMapping("/watchedlist")
-	public Set<Series> getWatchedListForUsername(Principal principal) {
-		
-		UserDto user = userService.findByUsername(principal.getName());
-		Long userId = user.getId();
+	@GetMapping("/history")
+	public Set<Series> getWatchedListForUsername(@PathVariable("userId") Long userId) {
 		
 		return watchlistsService.getWatchedSeriesList(userId);	
 	}
 	
-	public Series addSeriesToWatchedList(@PathVariable("seriesId") Long seriesId, Principal principal) {
-		
-		UserDto user = userService.findByUsername(principal.getName());
-		Long userId = user.getId();
+	@PostMapping("/history/{seriesId}")
+	public Series addSeriesToWatchedList(@PathVariable("userId") Long userId, @PathVariable("seriesId") Long seriesId) {
 		
 		return watchlistsService.saveToWatchedSeriesList(userId, seriesId);
 	}
-
 }
